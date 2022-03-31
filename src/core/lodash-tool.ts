84c892d759f4bc877f4f7fa544c3b21b051e3dd4
陈靖 <jing.chen@xbongbong.com>
2022-03-31 19:26:55 +0800

    feat: lodash push

--- src/core/lodash-tool.ts
+++ src/core/lodash-tool.ts
@@ -1,24 +1,17 @@
-/*
- * @Author: jing.chen
- * @Date: 2021-10-26 14:29:01
- * @LastEditors: jing.chen
- * @LastEditTime: 2021-10-26 14:30:12
- * @Description:
- */
-import _get from "lodash/get";
-import _map from "lodash/map";
-import _uniq from "lodash/uniq";
-import _pick from "lodash/pick";
-import _omit from "lodash/omit";
-import _isNaN from "lodash/isNaN";
-import _property from "lodash/property";
-import _findIndex from "lodash/findIndex";
-import _findLastIndex from "lodash/findLastIndex";
-import _trim from "lodash/trim";
-import _isEmpty from "lodash/isEmpty";
-import _isEqual from "lodash/isEqual";
-import _debounce from "lodash/debounce";
-import _throttle from "lodash/throttle";
+import _get from 'lodash/get'
+import _map from 'lodash/map'
+import _uniq from 'lodash/uniq'
+import _pick from 'lodash/pick'
+import _omit from 'lodash/omit'
+import _isNaN from 'lodash/isNaN'
+import _property from 'lodash/property'
+import _findIndex from 'lodash/findIndex'
+import _findLastIndex from 'lodash/findLastIndex'
+import _trim from 'lodash/trim'
+import _isEmpty from 'lodash/isEmpty'
+import _isEqual from 'lodash/isEqual'
+import _debounce from 'lodash/debounce'
+import _throttle from 'lodash/throttle'
 
 export {
   _get,
@@ -34,5 +27,5 @@ export {
   _isEmpty,
   _isEqual,
   _debounce,
-  _throttle
-};
+  _throttle,
+}
